76cc8aa4f09e8c494b3964dd288ad4d571167b05
Hrishikesh Zagade <hrishi.zagade@gmail.com>
2023-09-24 09:16:32 +0530

    23-09-23

--- automation/ProcessMonitor.py
+++ automation/ProcessMonitor.py
@@ -7,9 +7,10 @@ def process_display():
     for process in psutil.process_iter():
         try:
             process_info = process.as_dict(attrs=['pid', 'name', 'username'])
+            process_info['vms'] = process.memory_info().vms / (1024 * 1024)
             list_process.append(process_info)
 
-        except (psutil.NoSuchProcess, psutil.ZombieProcess, psutil.NoSuchProcess):
+        except (psutil.NoSuchProcess, psutil.ZombieProcess, psutil.NoSuchProcess, psutil.AccessDenied):
             pass
 
     return list_process
